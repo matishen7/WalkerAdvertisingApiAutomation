666df68533a06bb65e3301167313783b6164220e
matishen7 <ishenbaev7@gmail.com>
2024-08-13 14:45:50 -0500

    DeleteConsumer_ShouldReturnSuccess_And_Validate

--- DeleteConsumerTests.cs
+++ DeleteConsumerTests.cs
@@ -27,6 +27,23 @@ namespace WalkerAdvertisingApiAutomation
             Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
         }
 
+        [TestMethod]
+        public async Task DeleteConsumer_ShouldReturnSuccess_And_Validate()
+        {
+            var id = await CreateConsumerAsync();
+            var request = new RestRequest($"/api/Consumer/{id}", Method.Delete);
+
+            var response = await _client.ExecuteAsync(request);
+
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+            request = new RestRequest("/api/Consumer", Method.Get);
+            var allCustomersResponse = await _client.ExecuteAsync<List<ContactInfo>>(request);
+            Assert.AreEqual(HttpStatusCode.OK, allCustomersResponse.StatusCode);
+            Assert.IsNotNull(allCustomersResponse.Data);
+            Assert.IsInstanceOfType(allCustomersResponse.Data, typeof(List<ContactInfo>));
+            Assert.IsTrue(allCustomersResponse.Data.Find(x => x.Id == id) == null, $"There should be no customer found with id {id}");
+        }
+
         private async Task<int> CreateConsumerAsync()
         {
             var newConsumer = _fixture.Create<ContactInfo>();
